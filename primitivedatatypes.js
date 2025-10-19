4819a53077492d4360b4e66db052732fdd4645f7
Srimalar-1994 <lilappleheadz@gmail.com>
2025-10-19 13:37:12 +0530

    Add files via upload

---
+++ primitivedatatypes.js
@@ -0,0 +1,11 @@
+let userName = "John"         
+let organization = "ABCD"
+let contactNumber = 1234567890
+let knowsAutomation = true
+let usesPlaywright
+
+console.log("User Name: " + userName)
+console.log("Organization: " + organization)
+console.log("Contact Number: " + contactNumber)
+console.log("Knows Automation: " + knowsAutomation)
+console.log("Uses Playwright: " + typeof(usesPlaywright))
